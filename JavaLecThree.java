c426c71ccfadefeca983fc6c427beffd0e9d21d5
TRASJEPS <71234985+TRASJEPS@users.noreply.github.com>
2021-07-08 16:35:41 -0700

    java testing 5

---
+++ JavaLecThree.java
@@ -0,0 +1,9 @@
+// TRAVIS JEPSON 2021 JAVA
+
+public class JavaLecThree{
+    public static void main(String[] args) {
+
+        System.out.println("Lecture 3");
+
+    }
+}
---
+++ JavaLecTwo.java
@@ -0,0 +1,9 @@
+// TRAVIS JEPSON 2021 JAVA
+
+public class JavaLecTwo{
+    public static void main(String[] args) {
+
+        System.out.println("Lecture 2");
+
+    }
+}
